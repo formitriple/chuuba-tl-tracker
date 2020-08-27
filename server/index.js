7af48f26af593543fdf07ea48483cce32c35bbd3
andy <n.andyheng@gmail.com>
2020-08-27 15:46:55 -0400

    create get all videos route

--- server/index.js
+++ server/index.js
@@ -3,7 +3,18 @@ const express = require("express")
 const app = express()
 const got = require("got")
 const pool = require("./db")
+const queries = require("./queries")
 const port = process.env.APP_PORT
+
+// get all videos
+app.get("/api/videos", async(req, res) => {
+    try {
+        const videos = await pool.query(queries.getVideos)
+        res.json(videos.rows)
+    } catch (error) {
+        res.json(error)
+    }
+})
 
 app.listen(port, () => {
     console.log(`server started on port ${port}`)
