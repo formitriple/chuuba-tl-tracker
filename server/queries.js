3e8bc30136914494489bd4d032ac90320b05344f
andy <n.andyheng@gmail.com>
2020-08-27 16:39:21 -0400

    add additional queries for POST video route

--- server/queries.js
+++ server/queries.js
@@ -9,7 +9,11 @@ const queries = {
         JOIN vid_vtuber ON video.v_id = vid_vtuber.v_id
         JOIN vtuber ON vtuber.vt_id = vid_vtuber.vt_id
         GROUP BY video.v_id
-    `
+    `,
+    insertVideo: "INSERT INTO video(title, thumbnail_url_hq, thumbnail_url_max, video_url) VALUES($1, $2, $3, $4)",
+    getInsertedVideo: "SELECT v_id FROM video WHERE title = $1",
+    insertToJoin: "INSERT INTO vid_vtuber(v_id, vt_id) VALUES($1, $2)",
+    getVtuber: "SELECT * FROM vtuber WHERE name = $1"
 }
 
 module.exports = queries;
