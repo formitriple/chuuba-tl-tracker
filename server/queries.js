ba9ddc9b1ac4415dfc47db74466f3f1c93eaf077
andy <n.andyheng@gmail.com>
2020-08-28 09:36:48 -0400

    create query for filtering videos against video_includes array column

--- server/queries.js
+++ server/queries.js
@@ -10,10 +10,23 @@ const queries = {
         JOIN vtuber ON vtuber.vt_id = vid_vtuber.vt_id
         GROUP BY video.v_id
     `,
-    insertVideo: "INSERT INTO video(title, thumbnail_url_hq, thumbnail_url_max, video_url) VALUES($1, $2, $3, $4)",
+    getFilteredVideos: `
+        SELECT v.title, v.video_url, v.thumbnail_url_hq, v.thumbnail_url_max, json_agg(json_build_object(
+            'name', name,
+            'channel_url', channel_url,
+            'icon_url', icon_url)
+        ) AS vtubers
+        FROM video v
+        JOIN vid_vtuber vvt ON v.v_id = vvt.v_id
+        JOIN vtuber vt ON vt.vt_id = vvt.vt_id
+        WHERE $1 && v.video_includes
+        GROUP BY v.v_id
+        HAVING COUNT(DISTINCT vt.vt_id) >= $2
+    `,
+    insertVideo: "INSERT INTO video(title, thumbnail_url_hq, thumbnail_url_max, video_url, video_includes) VALUES($1, $2, $3, $4, $5)",
     getInsertedVideo: "SELECT v_id FROM video WHERE title = $1",
     insertToJoin: "INSERT INTO vid_vtuber(v_id, vt_id) VALUES($1, $2)",
-    getVtuber: "SELECT * FROM vtuber WHERE name = $1"
+    getVtuber: "SELECT * FROM vtuber WHERE name = $1",
 }
 
 module.exports = queries;
